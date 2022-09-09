075e199764568eb1d34cf0b4d1503add1c9a0463
iaceutest <113177164+iaceutest@users.noreply.github.com>
2022-09-09 17:10:22 +0530

    Create S3.tf

---
+++ S3.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+ # required_version = ">= 0.14.9"
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+ #text
+
+resource "aws_s3_bucket" "app_server" {
+  bucket = "examplebuckettftest"
+  acl    = "public-read-write"
+  versioning {
+    enabled = true
+  }
+  logging {
+    target_bucket = "pavantestbucket"
+    target_prefix = "log/"
+    }
+}
